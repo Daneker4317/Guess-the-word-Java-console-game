6817e82abdd2c53bbe28a10cec71799053522e4f
daneker <kkraken2005@gmail.com>
2023-02-18 11:40:26 +0600

    Java game

--- src/main/java/daneker/code/Game.java
+++ src/main/java/daneker/code/Game.java
@@ -1,9 +1,6 @@
 package daneker.code;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.*;
 
 public class Game {
@@ -11,44 +8,87 @@ public class Game {
     private static final File fileGetWordsToGuess = new File("C:\\Users\\Данекер\\Desktop\\daneker.txt");
     private static final File fileToWriteGuessedWords = new File("C:\\Users\\Данекер\\Desktop\\result.txt");
     private static final ReaderWriter readerWriter = new ReaderWriter();
+    private static GameOver gameOver = new GameOver();
+
+    private static int chooseLevel() {
+        System.out.println("{GUESS THE WORD}");
+        System.out.println("\n" +
+                "[4] - all letters are closed\n" +
+                "[3] - open one random letter\n" +
+                "[2] - open two random letters\n" +
+                "[1]- open three random letters");
+        System.out.println("choose level:");
+        int level = scanner.nextInt();
+        return level;
+    }
 
     public void start() throws Exception {
         List<String> guessed = readerWriter.getWordsFromFile(fileToWriteGuessedWords);
-        List<String> toGuess =readerWriter.getWordsFromFile(fileGetWordsToGuess);
-        game(guessed, toGuess);
+        List<String> toGuess = readerWriter.getWordsFromFile(fileGetWordsToGuess);
+        game(guessed, toGuess, chooseLevel());
     }
 
-    private static void game(List<String> guessedWords, List<String> wordsToGuess) throws Exception {
+    private static void game(List<String> guessedWords, List<String> wordsToGuess, int openedLetters) throws Exception {
         String wordToGuess = randomWord(wordsToGuess, guessedWords);
-        readerWriter.writeGuessedWordToFile(wordToGuess, guessedWords , fileToWriteGuessedWords);
-        char[] word = new char[wordToGuess.length()];
-        Arrays.fill(word, '*');
+        readerWriter.writeGuessedWordToFile(wordToGuess, guessedWords, fileToWriteGuessedWords);
+        char[] word = getFilledArray(openedLetters, wordToGuess);
         System.out.println("enter quantity of chances to guess word");
         int amountOfChances = scanner.nextInt();
         boolean win = input(amountOfChances, wordToGuess, word);
-        if(!win)
-            System.out.println("Game over! The word was:" + wordToGuess);
+        if (!win) {
+            gameOver.fillThird();
+            System.out.println("The word was:" + wordToGuess);
+        }
     }
 
-    private static boolean input(int c, String wordToGuess, char[] arr) {
-        System.out.println("be careful. You have only [" + c + "] chances");
+    private static char[] getFilledArray(int n, String wordToGuess) {
+        char[] arr = new char[wordToGuess.length()];
+        Arrays.fill(arr, '*');
+        int closedLetters = arr.length - n;
+        int currentClosedLetters = 0;
+        while (closedLetters != currentClosedLetters) {
+            int index = new Random().nextInt(wordToGuess.length());
+            arr[index] = wordToGuess.charAt(index);
+            currentClosedLetters = countLetters(arr);
+        }
+        return arr;
+    }
+
+    private static int countLetters(char[] arr) {
+        int counter = 0;
+        for (char ch : arr) {
+            if (Character.isLetter(ch)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
+    private static boolean input(int chance, String wordToGuess, char[] arr) {
+        System.out.println("be careful. You have only [" + chance + "] chances");
         System.out.println(String.valueOf(arr));
-        while (c > 0) {
+        while (chance > 0) {
+            if(chance == wordToGuess.length() / 3){
+                gameOver.fillSecond();
+            } else if (chance == (wordToGuess.length() * 2) / 3) {
+                gameOver.fillFirst();
+            }
             if (String.valueOf(arr).indexOf("*") < 0) {
-                won(wordToGuess, c);
+                won(wordToGuess, chance);
                 return true;
             }
             System.out.print(">>>");
             String str = scanner.next();
             if (str.equals(wordToGuess)) {
-                won(wordToGuess, c);
+                won(wordToGuess, chance);
                 return true;
             }
             if (!wordToGuess.contains(str) && str.length() == 1)
                 System.out.println("There is no such letter!");
             convertAndDisplay(arr, str.charAt(0), wordToGuess);
-            System.out.println("chances:" + (c - 1));
-            c--;
+            System.out.println("chances:" + (chance - 1));
+            chance--;
         }
         return false;
     }
@@ -70,6 +110,4 @@ public class Game {
         words.stream().filter(word -> !pastWords.contains(word)).forEach(word -> list.add(word));
         return list.get(new Random().nextInt(list.size()));
     }
-
-
 }
---
+++ src/main/java/daneker/code/GameOver.java
@@ -0,0 +1,38 @@
+package daneker.code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameOver {
+
+    public void fillFirst(){
+        System.out.println("----------");
+        System.out.println("|/       |");
+        System.out.println("|        o");
+        System.out.println("|         ");
+        System.out.println("|         ");
+        System.out.println("|         ");
+        System.out.println("|_________");
+    }
+    public  void fillSecond(){
+        System.out.println("----------");
+        System.out.println("|/       |");
+        System.out.println("|        o");
+        System.out.println("|       /|/");
+        System.out.println("|         ");
+        System.out.println("|         ");
+        System.out.println("|_________");
+    }
+
+    public  void fillThird(){
+        System.out.println("----------");
+        System.out.println("|/       !");
+        System.out.println("|        o");
+        System.out.println("|       /|/");
+        System.out.println("|        |");
+        System.out.println("|       / /");
+        System.out.println("|_________");
+        System.out.println("game over :(");
+    }
+
+}
